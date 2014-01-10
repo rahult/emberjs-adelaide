fb48836159529c82f36cd5e6b67c568e22ccc7eb
Rahul Trikha <rahul.trikha@gmail.com>
2014-01-10 11:54:18 +1030

    Added basic styles

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,7 +1,7 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 EmberjsAdelaide.Router.map(function() {
-  this.resource('presentations', { path: "/" }, function () {
+  this.resource('presentations', { path: "/presentations" }, function () {
     this.resource('presentation', { path: '/:presentation_id' });
   });
 });
