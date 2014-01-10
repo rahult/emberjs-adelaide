ad5dfd723009d078af4b788b6b850fd530a984de
Rahul Trikha <rahul.trikha@gmail.com>
2014-01-10 10:36:40 +1030

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberjsAdelaide::Application.config.secret_key_base = '9cf5eccedc75fca0a6777622cf5a2a78a002addc2f46ff0c4f6899504c79c19d52d84fd94d9a0671fd9020fcf294599f48b0ad27bad3fe8241ef283a09cc220d'
